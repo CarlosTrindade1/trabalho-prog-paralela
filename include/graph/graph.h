5ac8d89995ab24740b125f20df4aebc4cb58099b
Carlos Trindade <carlos.trindade@ufms.br>
2024-09-20 22:12:11 -0400

    Finalizando a implementação do algoritmo paralelo de contagem de clicks

--- include/graph/graph.h
+++ include/graph/graph.h
@@ -4,33 +4,42 @@
 #include <stdlib.h>
 #include <map>
 #include <vector>
+#include <math.h>
 
 using namespace std;
-
-struct Edge {
-    int id;
-    Edge* next;
-};
-
-struct Vertex {
-    int id;
-    Edge* edges;
-};
 
 class Graph {
     public:
         Graph(char *file_name);
         int count_clicks_serial(int k);
+        int count_clicks_parallel(int k, int num_threads);
         void print();
 
     private:
+        struct Edge {
+            int id;
+            Edge* next;
+        };
+
+        struct Vertex {
+            int id;
+            Edge* edges;
+        };
+        struct thread_args {
+            Graph *graph;
+            vector<vector<int>> *clicks;
+            int k;
+            int counter;
+        };
+
         int num_vertices;
         Vertex* vertices;
 
+        static void* count_clicks_entry(void *args);
+        int count_clicks(vector<vector<int>> clicks, int k);
         map<int, int> rename_vertices(char* file_name_graph);
         void read_graph(char* file_name_graph, map<int, int> new_ids);
         bool is_on_click(vector<int> click, int vertex);
         bool makes_a_click(vector<int> click, int vertex);
-        int count_clicks(vector<vector<int>> clicks, int k);
         bool is_neighbor(int vertex_1, int vertex_2);
 };
--- src/graph/graph-clicks.cpp
+++ src/graph/graph-clicks.cpp
@@ -1,5 +1,14 @@
 #include <graph.h>
 
+void print_clicks(vector<vector<int>> clicks) {
+    for (int i = 0; i < clicks.size(); i++) {
+        for (int j = 0; j < clicks[i].size(); j++) {
+            cout << clicks[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
 int Graph::count_clicks(vector<vector<int>> clicks, int k) {
     int counter = 0;
 
@@ -41,9 +50,16 @@ int Graph::count_clicks(vector<vector<int>> clicks, int k) {
     return counter;
 }
 
+void* Graph::count_clicks_entry(void *args) {
+    thread_args *thread_args = static_cast<struct thread_args *>(args);
+    int counter = thread_args->graph->count_clicks(*thread_args->clicks, thread_args->k);
+    thread_args->counter = counter;
+    pthread_exit(NULL);
+    return NULL;
+}
+
 int Graph::count_clicks_serial(int k) {
     vector<vector<int>> clicks(num_vertices, vector<int>(1));
-    int counter = 0;
     
     // Add all vertices to the clicksj array
     for (int i = 0; i < num_vertices; i++) {
@@ -51,7 +67,48 @@ int Graph::count_clicks_serial(int k) {
         clicks[i].push_back(vertices[i].id);
     }
 
-    counter = count_clicks(clicks, k);
+    int counter = count_clicks(clicks, k);
+
+    return counter;
+}
+
+int Graph::count_clicks_parallel(int k, int num_threads) {
+    vector<vector<int>> clicks(num_vertices, vector<int>(1));
+    int counter = 0;
+
+    // Add all vertices to the clicks array
+    for (int i = 0; i < num_vertices; i++) {
+        clicks[i].pop_back();
+        clicks[i].push_back(vertices[i].id);
+    }
+    
+    pthread_t *threads = new pthread_t[num_threads];
+
+    int vertices_per_thread = ceil(num_vertices / num_threads);
+
+    thread_args *args = new thread_args[num_threads];
+
+    for (int i = 0; i < num_threads; i++) {
+        int start = i * vertices_per_thread;
+        int end = (i + 1) * vertices_per_thread;
+
+        if (end > num_vertices)
+            end = num_vertices;
+
+        vector<vector<int>> *clicks_thread = new vector<vector<int>>(clicks.begin() + start, clicks.begin() + end);
+
+        args[i].clicks = clicks_thread;
+        args[i].k = k;
+        args[i].counter = 0;
+        args[i].graph = this;
+
+        pthread_create(&threads[i], NULL, count_clicks_entry, (void *) &args[i]);
+    }
+
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(threads[i], NULL);
+        counter += args[i].counter;
+    }
 
     return counter;
 }
